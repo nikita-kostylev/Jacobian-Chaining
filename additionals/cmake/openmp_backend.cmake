3a1ae72ae24fe17b1e910fa60a5ef164d610f616
Adil-Ahmadli <adil.ahmadli@proton.me>
2026-01-23 23:16:36 +0100

    fix

--- additionals/cmake/openmp_backend.cmake
+++ additionals/cmake/openmp_backend.cmake
@@ -53,7 +53,7 @@ string(REPLACE " " ";" OpenMP_CXX_FLAGS "${OpenMP_CXX_FLAGS}")
 
 # Specific for nvidia gpus...
 if (JCDP_OPENMP_GPU)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -g")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda")
 endif()
 
 # **************************************************************************** #
