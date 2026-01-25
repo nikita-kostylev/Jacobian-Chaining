f1d2d8d4f965ddb1b0d878215c20c0672600532d
Adil-Ahmadli <adil.ahmadli@proton.me>
2026-01-25 14:05:47 +0100

    fix

--- include/jcdp/optimizer/branch_and_bound.hpp
+++ include/jcdp/optimizer/branch_and_bound.hpp
@@ -186,7 +186,7 @@ class BranchAndBoundOptimizer : public Optimizer, public util::Timer {
                if (m_makespan > new_makespan) {
                   std::println("Updated m_makespan var: {}", m_makespan);
                   std::println(
-                       "Updated makespan() fun: {}", bnb_seq_gpu.makespan());
+                       "Updated makespan() fun: {}", final_sequence.makespan());
                   m_optimal_sequence = final_sequence;
                   m_makespan = new_makespan;
                   m_updated_makespan++;
