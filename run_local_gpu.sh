5da5b18fd5436b58e63e80a39d753d021ead58d1
Adil-Ahmadli <adil.ahmadli@proton.me>
2026-01-25 17:33:03 +0100

    intrinsics to script

--- run_local_gpu.sh
+++ run_local_gpu.sh
@@ -1,3 +1,3 @@
-git pull && module purge && module load intel/2023b && module load Clang/18.1.2-CUDA-12.3.0 && cmake -B build -S . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DJCDP_USE_OPENMP=ON && cmake --build build && CUDA_VISIBLE_DEVICES=3 ./build/bin/jcdp ./additionals/configs/config.in
+git pull && module purge && module load intel/2023b && module load Clang/18.1.2-CUDA-12.3.0 && cmake -B build -S . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DJCDP_USE_OPENMP=ON -DENABLE_NATIVE_INTRINSICS=ON && cmake --build build && CUDA_VISIBLE_DEVICES=3 ./build/bin/jcdp ./additionals/configs/config.in
 
 LIBOMPTARGET_INFO=-1 CUDA_VISIBLE_DEVICES=3 ./build/bin/jcdp ./additionals/configs/config.in
